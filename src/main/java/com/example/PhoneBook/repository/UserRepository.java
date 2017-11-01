ba813291dee65e0825492d189014a85a3048c76e
evgeniya.opanasenko@outlook.com <evgeniya.opanasenko@outlook.com>
2017-11-01 11:16:13 +0200

    init commit

---
+++ src/main/java/com/example/PhoneBook/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.PhoneBook.repository;
+
+import com.example.PhoneBook.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByName(String name);
+}
---
+++ src/main/java/com/example/PhoneBook/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.PhoneBook.service;
+
+
+import com.example.PhoneBook.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User findById(Long id);
+
+    User findByName(String name);
+
+    void saveUser(User user);
+
+    void updateUser(User user);
+
+    void deleteUserById(Long id);
+
+    void deleteAllUsers();
+
+    List<User> findAllUsers();
+
+    boolean isUserExist(User user);
+}
